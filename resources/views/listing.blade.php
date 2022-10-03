2c2163f3d3013c6b50ee311e678572a5b1201b78
Krushil Naik <krushilnaik96@gmail.com>
2022-10-03 19:34:26 -0400

    Add tags

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -5,13 +5,21 @@ $tag_list = explode(',', $tags);
 @endphp
 
 @section('content')
-  <h2>{{ $title }}</h2>
-  <h3>{{ $company }}</h3>
-  <ul class="flex gap-4">
-    @foreach ($tag_list as $tag)
-      <li class="px-2 py-1 rounded-full bg-slate-600 text-slate-100">{{ $tag }}</li>
-    @endforeach
-  </ul>
-  <p>{{ $description }}</p>
-  <a href="/listings/{{ $id }}/edit">Edit Listing</a>
+  <div class="prose">
+    <h2>Position: {{ $title }}</h2>
+    <h3>Company: {{ $company }}</h3>
+    <h4 class="font-bold">Tech Stack: </h4>
+    <ul class="flex gap-4 list-none">
+      @foreach ($tag_list as $tag)
+        <li class="px-2 py-1 rounded-full bg-slate-600 text-slate-100">{{ $tag }}</li>
+      @endforeach
+    </ul>
+    <span>Job Description: </span>
+    <p>{{ $description }}</p>
+    <hr class="my-5">
+    <a class="bg-sky-500 p-2 w-fit rounded-lg font-semibold text-slate-100 no-underline"
+      href="/listings/{{ $id }}/edit">
+      Edit Listing
+    </a>
+  </div>
 @endsection
