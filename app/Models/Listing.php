564ee537359ea3109c6c04dc7385d738cac06871
Krushil Naik <krushilnaik96@gmail.com>
2022-10-05 07:33:24 -0400

    Migrate to SQLite database

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -20,32 +20,4 @@ class Listing extends Model
     "user_id",
   ];
 
-  public function scopeTags($query, $tag)
-  {
-    if (!is_null($tag)) {
-      return $query->where("tag", "like", "%" . $tag . "%");
-    }
-
-    return $query;
-  }
-
-  public function scopeTitle($query, $title)
-  {
-    if (!is_null($title)) {
-      return $query->where("title", "like", "%" . $title . "%");
-    }
-
-    return $query;
-  }
-
-  // public function scopeFilter($query, array $filters)
-  // {
-  //   if ($filters['tag'] ?? false) {
-  //     $query->where('tags', 'like', '%' . request('tag') . '%');
-  //   }
-
-  //   if ($filters['search'] ?? false) {
-  //     $query->where('title', 'like', '%' . request('search') . '%');
-  //   }
-  // }
 }
