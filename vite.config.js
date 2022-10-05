f22ba9082d6e65908bb6b4d20973b50d2a42725b
Krushil Naik <krushilnaik96@gmail.com>
2022-10-05 08:06:22 -0400

    Revert Vite config, update Vercel PHP config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-  build: {
-    outDir: "dist",
-  },
+  //   build: {
+  //     outDir: "dist",
+  //   },
   plugins: [
     laravel({
       input: ["resources/css/app.css", "resources/js/app.js"],
       refresh: true,
-      buildDirectory: "dist",
+      //   buildDirectory: "dist",
     }),
   ],
 });
